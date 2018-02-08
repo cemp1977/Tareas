0ae0a807c6aec379a2dda74bc1db866322b6a3d2
Cristian Montes <cemp1977@gmail.com>
2018-02-08 10:25:59 -0600

    Tarea 3 js

---
+++ Introduccion/Tarea3.js
@@ -0,0 +1,43 @@
+var mes=10;
+switch (mes){
+  case 1:
+    alert("Invierno");
+    break;
+   case 2:
+    alert("Invierno");
+    break;
+   case 3:
+    alert("Primavera");
+    break;
+   case 4:
+    alert("Primavera");
+    break;
+   case 5:
+    alert("Primavera");
+    break;
+   case 6:
+    alert("Verano");
+    break;
+   case 7:
+    alert("Verano");
+    break;
+   case 8:
+    alert("Verano");
+    break;
+   case 9:
+    alert("Otoño");
+    break;
+  case 10:
+    alert("Otoño");
+    break;
+   case 11:
+    alert("Otoño");
+    break;
+   case 12:
+    alert("Invierno");
+    break;
+  default:
+    alert("Ingrese un numero entre 1 y 12");
+    break;
+}
+alert("Cristian Montes de Oca, 18008628");
